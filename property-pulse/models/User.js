1142fd7e3594162a42e7d3c7c5dd262ee924207a
Johnson Bhaskaran <johnsonb@live.in>
2025-11-18 15:42:59 +0530

    Done: User model created

---
+++ property-pulse/models/User.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      unique: [true, "Email already exists"],
+      required: [true, "Email is required"],
+    },
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+    },
+    image: {
+      type: String,
+    },
+    bookmarks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Property",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default User = mongoose.models.User || mongoose.model("User", userSchema);
