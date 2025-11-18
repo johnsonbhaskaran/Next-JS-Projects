58b229fbd70f48abff5d6ec0aee63501c66595a3
Johnson Bhaskaran <johnsonb@live.in>
2025-11-18 11:17:22 +0530

    Done: properties page completed

--- property-pulse/app/properties/page.jsx
+++ property-pulse/app/properties/page.jsx
@@ -1,6 +1,21 @@
-"use client";
+import PropertyCard from "@/components/PropertyCard";
+import properties from "@/properties.json";
 
 const PropertiesPage = () => {
-  return <div>PropertyPage</div>;
+  return (
+    <section className='px-4 py-6'>
+      <div className='m-auto px-4 py-6 container-xl lg:container'>
+        {properties.length === 0 ? (
+          <p>No properties found.</p>
+        ) : (
+          <div className='gap-6 grid grid-cols-1 md:grid-cols-3'>
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 export default PropertiesPage;
