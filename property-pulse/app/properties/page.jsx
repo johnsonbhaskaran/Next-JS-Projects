1eac81d92ece8b56c66aed1a03b277c1c9dc530c
Johnson Bhaskaran <johnsonb@live.in>
2025-11-18 15:57:36 +0530

    Done: Now data is fetch from DB

--- property-pulse/app/properties/page.jsx
+++ property-pulse/app/properties/page.jsx
@@ -1,7 +1,19 @@
 import PropertyCard from "@/components/PropertyCard";
-import properties from "@/properties.json";
+// import properties from "@/properties.json";
 
-const PropertiesPage = () => {
+async function fetchProperties() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
+    if (!res.ok) throw new Error("Failed to fetch data");
+    return res.json();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const PropertiesPage = async () => {
+  const properties = await fetchProperties();
+
   return (
     <section className='px-4 py-6'>
       <div className='m-auto px-4 py-6 container-xl lg:container'>
